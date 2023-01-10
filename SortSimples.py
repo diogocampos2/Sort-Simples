bdcd1244c7eea6cf8b501b2649f6b7760db3ddc4
Diogo <diogo_campos@ufms.br>
2023-01-10 18:23:48 -0400

    Criação do algoritmo

--- SortSimples.py
+++ SortSimples.py
@@ -6,29 +6,29 @@ cont3 = int(C)
 
 if A <= B and A <= C:
 	print(A)
-    elif B <= A and B <= C:
-	    print(B)
-    elif C <= A and C <=B:
-	    print(C)
+elif B <= A and B <= C:
+	print(B)
+elif C <= A and C <=B:
+	print(C)
 
 if A >= B and A <= C:
 	print(A)
-    elif A <= B and A >= C:
-	    print(A)
-    elif B >= A and B <= C:
-	    print(B)
-    elif B <= A and B >= C:
-	    print(B)
-    elif C >= A and C <= B:
-	    print(C)
-    elif C <= A and C >= B:
-	    print(C)
+elif A <= B and A >= C:
+	print(A)
+elif B >= A and B <= C:
+	print(B)
+elif B <= A and B >= C:
+	print(B)
+elif C >= A and C <= B:
+	print(C)
+elif C <= A and C >= B:
+	print(C)
 
 if A >= B and A >= C:
 	print(A)
-    elif B >= A and B >= C:
-	    print(B)
-    elif C >= A and C >= B:
-	    print(C)
+elif B >= A and B >= C:
+	print(B)
+elif C >= A and C >= B:
+	print(C)
 
 print(f"\n{cont1}\n{cont2}\n{cont3}")
